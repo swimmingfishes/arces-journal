445a3a9349b3afca4b57e4c602e0dc3d2c8312d7
ferdian <ferdianfariza@gmail.com>
2026-04-18 03:15:11 +0700

    feat: add support for blob storage and update environment variables
    - Added DATABASE_AUTH_TOKEN and BLOB_READ_WRITE_TOKEN to environment variables.
    - Integrated vercelBlobStorage plugin with conditional enabling based on BLOB_READ_WRITE_TOKEN.
    - Updated package dependencies in pnpm-lock.yaml to include new versions and peer dependencies.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -4,9 +4,12 @@ import { Plugin } from 'payload'
 import { GenerateTitle, GenerateURL } from '@payloadcms/plugin-seo/types'
 import { searchFields } from '@/search/fieldOverrides'
 import { beforeSyncWithSearch } from '@/search/beforeSync'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 import { Page, News } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
+
+const hasBlobToken = Boolean(process.env.BLOB_READ_WRITE_TOKEN)
 
 const generateTitle: GenerateTitle<News | Page> = ({ doc }) => {
   return doc?.title ? `${doc.title} | Arces News` : 'Arces News '
@@ -31,5 +34,13 @@ export const plugins: Plugin[] = [
         return [...defaultFields, ...searchFields]
       },
     },
+  }),
+  vercelBlobStorage({
+    enabled: hasBlobToken,
+    collections: {
+      media: true,
+    },
+    token: process.env.BLOB_READ_WRITE_TOKEN,
+    clientUploads: true,
   }),
 ]
